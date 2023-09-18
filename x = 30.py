5144f18dfd818338fc80c17a930e3298d21e9a86
SRAbhinay <124809897+SRAbhinay@users.noreply.github.com>
2023-09-18 13:23:20 +0530

    Add files via upload

---
+++ x = 30.py
@@ -0,0 +1,10 @@
+x = 10
+y = 10
+if x >= y:
+    print("x is greater than or equals to y")
+    if x == y:
+        print("x is equals to y")
+    else:
+        print("x is greater than y")
+else:
+    print("x is less than y")
