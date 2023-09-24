26a6bb8a8a4b0dc0757001c01ff75ada44fe4f95
S Raj Abhinay <124809897+SRAbhinay@users.noreply.github.com>
2023-09-24 12:44:54 +0530

    Add files via upload

---
+++ Sort & Searching/bucketsort.py
@@ -0,0 +1,14 @@
+def bucket_sort(arr):
+    max_val, min_val = max(arr), min(arr)
+    bucket_range = (max_val - min_val) / len(arr)
+    buckets = [[] for _ in range(len(arr))]
+
+    for num in arr:
+        index = int((num - min_val) / bucket_range)
+        buckets[index].append(num)
+
+    sorted_arr = []
+    for bucket in buckets:
+        insertion_sort(bucket)
+        sorted_arr.extend(bucket)
+    return sorted_arr
