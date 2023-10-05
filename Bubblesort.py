994e21a9d6a33426706e7d97cdfecc98ce24e06d
S Raj Abhinay <124809897+SRAbhinay@users.noreply.github.com>
2023-10-05 22:47:46 +0530

    Update Bubblesort.py

--- Bubblesort.py
+++ Bubblesort.py
@@ -3,7 +3,6 @@ def bubble_sort(arr):
     n = len(arr)
     for i in range(n):
         for j in range(0, n-i-1):
-            # Swap if the element found is greater than the next element
             if arr[j] > arr[j+1] :
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
